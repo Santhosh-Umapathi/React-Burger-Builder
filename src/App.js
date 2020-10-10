f22a360ca94c6e405350629590f97cbc2ba23c9f
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-10-10 11:02:10 +0200

    Layout & hoc wrapper added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Layout from './Layout/Layout';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>Test Page</p>
+        </Layout>
       </div>
     );
   }
